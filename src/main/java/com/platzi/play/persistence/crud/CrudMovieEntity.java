a9be78ddacff63710d0e11595f03c010984cd832
ares <giovaniarias06@gmail.com>
2025-09-15 18:00:18 -0500

    refactor: jpa repository

--- src/main/java/com/platzi/play/persistence/crud/CrudMovieEntity.java
+++ src/main/java/com/platzi/play/persistence/crud/CrudMovieEntity.java
@@ -1,8 +1,9 @@
 package com.platzi.play.persistence.crud;
 
 import com.platzi.play.persistence.entity.MovieEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+public interface CrudMovieEntity extends JpaRepository<MovieEntity, Long> {
 
 }
