59a22e821c7f399c58f5af1967caffa3844c6184
ares <giovaniarias06@gmail.com>
2025-09-26 01:07:27 -0500

    feat: MovieDto update con campos

--- src/main/java/com/platzi/play/domain/dto/MovieDto.java
+++ src/main/java/com/platzi/play/domain/dto/MovieDto.java
@@ -1,4 +1,15 @@
 package com.platzi.play.domain.dto;
 
-public record MovieDto() {
+import com.platzi.play.domain.Genre;
+
+import java.time.LocalDate;
+
+public record MovieDto(
+        String title,
+        Integer duration,
+        Genre genre,
+        LocalDate releaseDate,
+        Double rating,
+        boolean available
+) {
 }
