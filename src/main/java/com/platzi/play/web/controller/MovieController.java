89edc7661076d6046490bea13bee7d55c2de555c
ares <giovaniarias06@gmail.com>
2025-10-04 09:01:08 -0500

    feat: manejo con ResponseEntity

--- src/main/java/com/platzi/play/web/controller/MovieController.java
+++ src/main/java/com/platzi/play/web/controller/MovieController.java
@@ -1,31 +1,40 @@
-package com.platzi.play.web.controller;
+    package com.platzi.play.web.controller;
 
-import com.platzi.play.domain.dto.MovieDto;
-import com.platzi.play.domain.service.MovieService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+    import com.platzi.play.domain.dto.MovieDto;
+    import com.platzi.play.domain.service.MovieService;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+    import java.util.List;
 
-@RestController
-@RequestMapping("/movies")
-public class MovieController{
+    @RestController
+    @RequestMapping("/movies")
+    public class MovieController{
 
-    private final MovieService movieService;
+        private final MovieService movieService;
 
-    public MovieController(MovieService movieService) {
-        this.movieService = movieService;
+        public MovieController(MovieService movieService) {
+            this.movieService = movieService;
+        }
+
+        @GetMapping
+        public ResponseEntity<List<MovieDto>> getAll() {
+            List<MovieDto> movies = this.movieService.getAll();
+            if (movies == null || movies.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(movies);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<MovieDto> getById(@PathVariable Long id) {
+            MovieDto movieDto = this.movieService.getById(id);
+            if(movieDto == null){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(movieDto);
+        }
     }
-
-    @GetMapping
-    public List<MovieDto> getAll() {
-        return this.movieService.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public MovieDto getById(@PathVariable Long id) {
-        return this.movieService.getById(id);
-    }
-}
